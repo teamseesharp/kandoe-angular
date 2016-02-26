be97d0cd733a8152c48a0ac0f4c888bb562fadfa
cas decelle <cas.decelle@gmail.com>
2016-02-26 15:08:13 +0100

    fixed routing and refresh

--- src/angular/scripts/boot.ts
+++ src/angular/scripts/boot.ts
@@ -2,8 +2,9 @@
 import {AppComponent} from './app';
 import {provide} from 'angular2/core';
 import {HTTP_PROVIDERS} from "angular2/http";
-import {ROUTER_PROVIDERS, APP_BASE_HREF} from 'angular2/router';
+import { ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy } from 'angular2/router';
 
-bootstrap(AppComponent,
-    [ROUTER_PROVIDERS, HTTP_PROVIDERS, provide(APP_BASE_HREF, { useValue: "/" })]
-);
+bootstrap(AppComponent, [
+    ROUTER_PROVIDERS,
+    provide(LocationStrategy, { useClass: HashLocationStrategy })
+]);
--- src/angular/wwwroot/appScripts/boot.js
+++ src/angular/wwwroot/appScripts/boot.js
@@ -1,7 +1,9 @@
 var browser_1 = require('angular2/platform/browser');
 var app_1 = require('./app');
 var core_1 = require('angular2/core');
-var http_1 = require("angular2/http");
 var router_1 = require('angular2/router');
-browser_1.bootstrap(app_1.AppComponent, [router_1.ROUTER_PROVIDERS, http_1.HTTP_PROVIDERS, core_1.provide(router_1.APP_BASE_HREF, { useValue: "/" })]);
+browser_1.bootstrap(app_1.AppComponent, [
+    router_1.ROUTER_PROVIDERS,
+    core_1.provide(router_1.LocationStrategy, { useClass: router_1.HashLocationStrategy })
+]);
 //# sourceMappingURL=boot.js.map
