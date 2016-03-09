5b754b1eb02ab12e1e839ba14c2a56173e13941d
Bennie Helsen <helsen.bennie@hotmail.be>
2016-03-09 23:29:15 +0100

    fix modals

--- Web/resources/js/timeline.js
+++ Web/resources/js/timeline.js
@@ -22,51 +22,5 @@
                 'width': '80%'
             }, 500);
         });
-        if ($('#switch1').not(':checked')) {
-            $('.modal').wrap('<div class="mask"></div>')
-            $('.mask').click(function () {
-                $(this).fadeOut(300);
-                $('.mask article').animate({
-                    'top': '-100%'
-                }, 300)
-            });
-
-            $('.dot').click(function () {
-                var modal = $(this).attr('id');
-                $('.mask').has('article.' + modal).fadeIn(300);
-                $('.mask article.' + modal).fadeIn(0).animate({
-                    'top': '10%'
-                }, 300);
-            });
-        }
-        $("#switch1").click(function () {
-            if ($('#switch1').is(':checked')) {
-                $('.modal').unwrap('<div class="mask"></div>');
-                $('.modal').hide();
-                $('.modal').addClass('nobox');
-                $('.dot').click(function () {
-                    var modal = $(this).attr('id');
-                    $('article.nobox').hide()
-                    $('article.nobox.' + modal).fadeIn(200)
-                });
-            } else {
-                $('article').removeClass("nobox");
-                $('.modal').wrap('<div class="mask"></div>')
-                $('.mask').click(function () {
-                    $(this).fadeOut(300);
-                    $('.mask article').animate({
-                        'top': '-100%'
-                    }, 300)
-                });
-
-                $('.dot').click(function () {
-                    var modal = $(this).attr('id');
-                    $('.mask').has('article.' + modal).fadeIn(300);
-                    $('.mask article.' + modal).fadeIn(0).animate({
-                        'top': '10%'
-                    }, 300);
-                });
-            }
-        })
     }, 500);
 });
