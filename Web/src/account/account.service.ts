d2b2c45258f30a1ac10650e7c91c00bef8f0ff00
cas decelle <cas.decelle@gmail.com>
2016-03-12 12:13:37 +0100

    eerste call, whooptidoo, nog problemen met json -> object conversie

--- Web/src/account/account.service.ts
+++ Web/src/account/account.service.ts
@@ -1,9 +1,7 @@
 ﻿import {Injectable}     from 'angular2/core';
 import {Observable}     from 'rxjs/Observable';
 import {AuthHttp} from 'angular2-jwt';
-import {Http, Response} from 'angular2/http';
-
-import {Account} from './model/account';
+import {Http, Response, Headers} from 'angular2/http';
 
 @Injectable()
 export class AccountService {
@@ -15,14 +13,11 @@ export class AccountService {
     }
 
     getAccount() {
-        var apiURL = 'http://kandoe-api.azurewebsites.net/api/accounts/by-auth0-user-id/' + localStorage.getItem('profile');
-        return this.authHttp.get(apiURL)
-            .map(res => <Account> res.json().data)
-            .catch(this.handleError);
-    }
-
-    private handleError(error: Response) {
-        console.error(error);
-        return Observable.throw(error.json().error || 'Server error');
+        var header = new Headers();
+        header.append('Accept', 'text/json');
+        header.append('Content-Type', 'application/json');
+        var profile = JSON.parse(localStorage.getItem('profile'));
+        var apiURL = 'http://kandoe-api.azurewebsites.net/api/accounts/by-auth0-user-id/' + profile.user_id;
+        return this.authHttp.get(apiURL, { headers: header });
     }
 }
--- Web/src/account/profile.component.ts
+++ Web/src/account/profile.component.ts
@@ -7,22 +7,29 @@ import {BodyContentComponent} from '../defaultcomponents/body-content.component'
 import {SidebarComponent} from '../defaultcomponents/sidebar.component';
 
 import {Account} from './model/account';
+import {AccountDTO} from './model/account.dto';
 import {AccountService} from './account.service';
 
 @Component({
     directives: [HeadingComponent, BodyContentComponent, SidebarComponent],
-    templateUrl: 'src/account/profile.html'
+    templateUrl: 'src/account/profile.html',
+    providers: [AccountService]
 })
 
 export class ProfileComponent {
     model = new Account("helsen.bennie@hotmail.be", "Test1234");
     submitted = false;
-    private account: Account;
+    private accountDTO: AccountDTO = new AccountDTO();
 
     constructor(private _router: Router, private _accountService: AccountService) {
         if (!tokenNotExpired()) { this._router.navigate(['Login']); }
-        var acc = _accountService.getAccount();
-        console.log(acc);
+        _accountService.getAccount()
+            .subscribe(
+            data => console.log(data.json()),
+            err => console.log(err),
+            () => console.log('Complete')
+        );
+        console.log(this.accountDTO);
     }
 
     onSubmit() {
