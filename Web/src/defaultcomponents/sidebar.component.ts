cf3b6c25296d04918d5ec3b8ffd3dda7a697c197
cas decelle <cas.decelle@gmail.com>
2016-03-15 11:53:33 +0100

    jos

--- Web/src/defaultcomponents/sidebar.component.ts
+++ Web/src/defaultcomponents/sidebar.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿import {Component, View} from 'angular2/core';
 import {ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
 
@@ -15,7 +14,7 @@ import {tokenNotExpired} from 'angular2-jwt';
     selector: 'sidebar',
     directives: [ROUTER_DIRECTIVES],
     templateUrl: 'src/defaultcomponents/sidebar.html',
-    providers: [AccountService] 
+    providers: [AccountService]
 })
 
 export class SidebarComponent {
@@ -25,42 +24,11 @@ export class SidebarComponent {
     constructor(private _accountService: AccountService) {
         if (tokenNotExpired()) {
             _accountService.getAccountByUserId(localStorage.getItem('user_id'))
-            .subscribe(
-            data => this.account = _accountService.accountFromJson(data.json()),
-            err => console.log(err),
-            () => console.log('Sidebar complete')
-            );}
+                .subscribe(
+                data => this.account = _accountService.accountFromJson(data.json()),
+                err => console.log(err),
+                () => console.log('Sidebar complete')
+                );
+        }
     }
-=======
-﻿import {Component, View} from 'angular2/core';
-import {ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
-
-import {Session} from '../session/model/session';
-import {SessionType} from '../session/model/session';
-import {Organisation} from '../session/model/organisation';
-import {Theme} from '../session/model/theme';
-import {Card} from '../session/model/card';
-import {Account} from '../account/model/account';
-import {AccountService} from '../account/account.service';
-
-@Component({
-    selector: 'sidebar',
-    directives: [ROUTER_DIRECTIVES],
-    templateUrl: 'src/defaultcomponents/sidebar.html',
-    providers: [AccountService] 
-})
-
-export class SidebarComponent {
-
-    public account: Account = new Account();
-
-    constructor(private _accountService: AccountService) {
-        _accountService.getAccountByUserId(localStorage.getItem('user_id'))
-            .subscribe(
-            data => this.account = _accountService.accountFromJson(data.json()),
-            err => console.log(err),
-            () => console.log('Sidebar complete')
-        );
-    }
->>>>>>> origin/master
 }
