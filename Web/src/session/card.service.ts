eda0b062e9f027c4c16e0109670f9c372d0964fc
Joachim <joachim@gmail.com>
2016-03-15 09:27:01 +0100

    begin implementatie card service

---
+++ Web/src/session/card.service.ts
@@ -0,0 +1,26 @@
+﻿import {Injectable}     from 'angular2/core';
+import {Observable}     from 'rxjs/Observable';
+import {AuthHttp} from 'angular2-jwt';
+import {Response, Headers} from 'angular2/http';
+
+import {Card} from './model/card';
+
+@Injectable()
+export class CardService {
+
+    public apiPrefix: string = 'http://localhost:51787/';
+    //public apiPrefix: string = 'http://kandoe-api.azurewebsites.net/';
+    public header: Headers = new Headers();
+
+    constructor(private authHttp: AuthHttp) {
+        this.header.append('Accept', 'text/json');
+        this.header.append('Content-Type', 'application/json');
+    }
+
+    public getCardsBySubtheme() {
+        var apiURL = this.apiPrefix + 'api/selection-cards/by-subtheme/' ;
+        return this.authHttp.get(apiURL, { headers: this.header });
+    }
+
+   
+}
--- Web/src/session/cards.component.ts
+++ Web/src/session/cards.component.ts
@@ -1,5 +1,5 @@
 ﻿import {Component} from 'angular2/core';
-import {Router} from 'angular2/router';
+import {Router, RouteParams} from 'angular2/router';
 import {tokenNotExpired} from 'angular2-jwt';
 
 import {HeadingComponent} from '../defaultcomponents/heading.component';
@@ -7,6 +7,7 @@ import {BodyContentComponent} from '../defaultcomponents/body-content.component'
 import {SidebarComponent} from '../defaultcomponents/sidebar.component';
 
 import {Card} from './model/card';
+import {CardService} from './card.service';
 
 @Component({
     directives: [HeadingComponent, BodyContentComponent, SidebarComponent],
@@ -19,9 +20,15 @@ export class CardsComponent {
     model = new Card();
     submitted = false;
     cardToChange: Card;
-    
-    constructor(private _router: Router) {
+
+    constructor(private _router: Router, private _routeParams: RouteParams, private _cardService CardService) {
         if (!tokenNotExpired()) { this._router.navigate(['Login']); }
+        _cardService.getThemesByOrganisation(parseInt(this._routeParams.get('id')))
+            .subscribe(
+            data => this.card = _cardService.getCardsBySubtheme(data.json()),
+            err => console.log(err),
+            () => console.log('Complete theme')
+            );
         this.initializeCards();
     }
 
